31f5484c971a60b469fb226a337913de7f9cdd80
Rashadhusien <rashadhusein440@gmail.com>
2024-07-17 16:46:16 +0300

    first Upload

---
+++ src/api/servicecontent/controllers/servicecontent.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * servicecontent controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::servicecontent.servicecontent');
---
+++ src/api/servicecontent/services/servicecontent.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * servicecontent service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::servicecontent.servicecontent');
